71314fea9e747210908be366af2c54ed6247ad84
Gustavo Maysonnave Franck <acowatkme@hotmail.com>
2022-07-05 01:14:37 -0300

    uploading real world vue from vue mastery

---
+++ src/views/EventList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <event-card v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    EventService.getEvents().then((response) => {
+      this.events = response.data;
+    });
+  },
+};
+</script>
+
+<style></style>
